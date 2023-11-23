822284c82876c6996ad27ed80dd2442dcbf81c17
dembasow98 <dastech1998@gmail.com>
2023-11-23 13:35:45 +0300

    Estabilished the project build.

---
+++ build/assets/js/cf22ea98.3d324ce1.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwiki_wonder=self.webpackChunkwiki_wonder||[]).push([[5761],{3769:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
