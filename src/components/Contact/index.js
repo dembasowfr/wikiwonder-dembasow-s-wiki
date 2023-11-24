4e28bcc9bd75650fc02a5380b6fe89776c86fc1b
dembasow98 <dastech1998@gmail.com>
2023-11-24 23:17:16 +0300

    Done with form validation using react-from-hook

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -16,6 +16,7 @@ import { useThemeConfig } from '@docusaurus/theme-common';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+import { useForm } from 'react-hook-form';
 
 export default function Contact() {
 
@@ -36,6 +37,7 @@ export default function Contact() {
 
     
 
+
     // Variables
     const {siteConfig: {customFields},} = useDocusaurusContext();
 
@@ -53,8 +55,17 @@ export default function Contact() {
 
     const form = useRef();
     const [showRecaptcha, setShowRecaptcha] = useState(false);
+
+    const { register  ,setValue,  formState:{errors} ,  handleSubmit} = useForm(
+        {mode: "all"}
+    );
+
+    console.log("Form Errors: ",errors);
+
     const sendEmail = (e) => {
         e.preventDefault();
+
+        
 
         emailjs.sendForm(emailjs_service_id, emailjs_template_id, form.current, emailjs_public_key)
         .then((result) => {
@@ -81,7 +92,7 @@ export default function Contact() {
     };
 
     
-    
+  
     
     return (
         
@@ -91,43 +102,77 @@ export default function Contact() {
                 <h2 className={styles.contactTitle}>CONTACT</h2>
                 <h3 className={styles.contactSubtitle}>Do you have any suggestion? Want to say something? Please do share it with me</h3>
                 <div className={styles.features}>
-                    <form className={styles.contactForm} ref={form} onSubmit={sendEmail}>
+                    <form 
+                        className={styles.contactForm} 
+                        ref={form} 
+                        onSubmit={
+                            handleSubmit(
+                                (data) => console.log(data)
+                            )
+                        }
+                    >
                         <div className={styles.formLabels}>
                             <div className={styles.formLabel}>
                                 <label>Name</label>
-                                <input type="text" name="from_name" value={formData.from_name} 
+                                <input 
+                                    {
+                                        ...register(
+                                            "from_name",  
+                                            {required:"Name is required!!!"},
+                                            {minLength: {value: 3, message: "Name must be at least 3 characters long!!!"}}
+                                        )
+                                    } 
+                                    type="text" name="from_name" value={formData.from_name} 
                                     placeholder='Your Lovely Name'
-                                    onChange={handleChange}/>
-                                <div className={styles.errorMessage}>Your name is required!!!</div>
+                                    onChange={handleChange}
+                                />
+                                {errors.from_name && <p className={styles.errorMessage}>{errors.from_name.message}</p>}
+                               
                             </div>
                             <div className={styles.formLabel}>
                                 <label>Surname</label>
-                                <input type="text" name="from_surname" placeholder='Your Beloved Surname' value={formData.from_surname} 
-                                    onChange={handleChange}/>
-                                <div className={styles.errorMessage}> Your surname is required!!!</div>
+                                <input 
+                                    {...register("from_surname", 
+                                        {required:"Surname is required!!!"},
+                                        {minLength: {value: 3, message: "Surname must be at least 3 characters long!!!"}}
+                                    )} type="text" name="from_surname" placeholder='Your Beloved Surname' value={formData.from_surname} 
+                                    onChange={handleChange}
+                                />
+                                {errors.from_surname && <p className={styles.errorMessage}>{errors.from_surname.message}</p>}
                             </div>
                             <div className={styles.formLabel}>
                                 <label>Email</label>
-                                <input type="email" name="from_email" placeholder="martinlutherking@gmail.com" value={formData.from_email} 
-                                    onChange={handleChange}/>
-                                <div className={styles.errorMessage}> Please enter your email address!!!</div>
+                                <input {...register("from_email", 
+                                    {required:"Email address is required!!!"},
+                                    {pattern: {value: /^\S+@\S+$/i, message: "Invalid email address!!!"}}
+                                    )} type="email" name="from_email" placeholder="martinlutherking@gmail.com" value={formData.from_email} 
+                                    onChange={handleChange}
+                                />
+                                {errors.from_email && <p className={styles.errorMessage}>{errors.from_email.message}</p>}
                             </div>
                         
                             <div className={styles.formLabel}>
                                 <label>Review</label>
-                                <textarea className={styles.feedback} type="text" name="message"
+                                <textarea {...register("message", {required:"Message can not be empty!!!"},
+                                    {minLength: {value: 10, message: "Message must be at least 10 characters long!!!"}}
+                                    )} className={styles.feedback} type="text" name="message"
                                     placeholder="Say Something Here..." value={formData.message}
-                                    onChange={handleChange} />
-                                <div className={styles.errorMessage}>Message can not be empty!!!</div>
+                                    onChange={handleChange} 
+                                />
+                                {errors.message && <p className={styles.errorMessage}>{errors.message.message}</p>}
                             </div>
                             <div className={styles.formLabel}>
                                 <div className={styles.recaptcha}>
                                     <ReCAPTCHA 
+                                        {...register("recaptcha", 
+                                            {required:"Please verify you are a human!!!"})
+                                        }
                                        name = "recaptcha"
                                        sitekey={recaptcha_site_key} 
-                                       onChange={(value) => setFormData({ ...formData, recaptcha: value })}
+                                       onChange={(value) => setValue("recaptcha", value, { shouldValidate: true })}
                                        theme={recaptchaTheme}
                                     />
+                                    {errors.recaptcha && <p className={styles.errorMessage}>{errors.recaptcha.message}</p>}
                                 </div>
                             </div>
 
